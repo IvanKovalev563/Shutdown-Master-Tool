8ca0c72cb932b8f26b0289f1390ce5deba64d7d7
Phantomchik563 <ivango36372@gmail.com>
2025-12-06 15:30:41 +0300

    Добавьте файлы проекта.

---
+++ Shutdown Master/FormMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shutdown_Master
+{
+    public partial class FormMain : Form
+    {
+        void cmd()
+        {
+
+        }
+
+        string[] appModes = { "Завершение работы", "Перезагрузка" };
+        public FormMain()
+        {
+            InitializeComponent();
+            comboBoxModes.DataSource = appModes;
+        }
+    }
+}
