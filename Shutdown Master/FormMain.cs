f911b3106044a4ad24b6db0e41e2adb75935114d
Phantomchik563 <ivango36372@gmail.com>
2025-12-18 16:58:36 +0300

    v0.3.1.1 build 181225 изменения:  - Добавлена иконка исполняемого (.exe) файла

--- Shutdown Master/FormMain.cs
+++ Shutdown Master/FormMain.cs
@@ -23,7 +23,7 @@ namespace Shutdown_Master
 
         public string verFormat()
         {
-            string buildDate = "161225"; // ДАТА БИЛДА
+            string buildDate = "181225"; // ДАТА БИЛДА
             string verString;
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
             if(version.Major > 0)
